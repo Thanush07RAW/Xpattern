7d4edebe2c3ef0df4a32b7fd8b9275e6034792db
Draken <draken0067@gmail.com>
2025-01-30 10:54:13 +0530

    Add files via upload

---
+++ Xpattern.java
@@ -0,0 +1,15 @@
+public class Xpattern {
+    public static void main(String[] args) {
+        int size = 5;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (j == i || j == size - i - 1) {
+                    System.out.print("X ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
